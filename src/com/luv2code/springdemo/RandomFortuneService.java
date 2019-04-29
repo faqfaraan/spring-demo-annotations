a951a76dd8dadb9057a28ae03788e0956dbccd7b
Faraan <faqfaraan@gmail.com>
2019-04-28 20:41:00 -0400

    first commit

---
+++ src/com/luv2code/springdemo/RandomFortuneService.java
@@ -0,0 +1,25 @@
+package com.luv2code.springdemo;
+
+import java.util.Random;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RandomFortuneService implements FortuneService {
+
+	private String[] data = {
+			"1",
+			"2",
+			"3"
+	};
+	
+	private Random myRandom = new Random();
+	
+	@Override
+	public String getFortune() {
+		
+		int index = myRandom.nextInt(data.length);
+		return data[index];
+	}
+
+}
---
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -0,0 +1,41 @@
+package com.luv2code.springdemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TennisCoach implements Coach {
+
+	@Autowired
+	@Qualifier("randomFortuneService")
+	private FortuneService fortuneService;
+	
+//	@Autowired
+//	public TennisCoach(FortuneService theFortuneService) {
+//		fortuneService = theFortuneService;
+//	}
+	
+	public TennisCoach() {
+		System.out.println("inside default constructor");
+	}
+	
+	@Override
+	public String getDailyWorkout() {
+		return "Practice your backhand volley";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
+	}
+
+//	@Autowired
+//	public void doSomeCrazyStuff(FortuneService fortuneService) {
+//		System.out.println("TennisCoach inside crazy method");
+//		this.fortuneService = fortuneService;
+//	}
+	
+	
+
+}
